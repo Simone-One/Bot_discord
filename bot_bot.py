289a3d9044c86af4ad3f72e431f285d1b6dac2f5
Simone-One <simone.carassale@gmail.com>
2025-11-17 16:46:59 +0100

    Aggiunti i comandi di meme, anatra e cane
    Aggiunti comandi per inviare meme e immagini di animali.

--- bot_bot.py
+++ bot_bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from gen_pass import *
+import os
+from script_importazioni_immagini import *
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -27,5 +29,21 @@ async def domanda(ctx):
 @bot.command()
 async def genera_password(ctx, pass_lenght = 21):
     await ctx.send(gen_pass(pass_lenght))
+
+@bot.command()
+async def meme(ctx):
+    await ctx.send(file=immagini_meme())
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una volta chiamato il comando duck, il programma richiama la funzione get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def dog(ctx):
+    '''Una volta chiamato il comando duck, il programma richiama la funzione get_duck_image_url'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run("TOKEN")
